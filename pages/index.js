2d34f44bacb8cc6f77c0f5fc19041cab2a20b247
EricEOL <lopes.eric051@gmail.com>
2022-11-28 21:43:52 -0300

    Save data in localStorage

--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,94 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-  const [] = useState(null);
+  const [botKey, setBotKey] = useState(null);
+  const [chatId, setChatId] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [postSlug, setPostSlug] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const SUCCESS_MESSAGE = "Seu post está sendo monitorado"
+  const ERROR_MESSAGE = "Seus parâmetros são inválidos";
+
+  function handleRegisterPostMonitor(event) {
+
+    event.preventDefault();
+    
+    const data = {
+      botKey,
+      chatId,
+      username,
+      postSlug
+    }
+
+    console.log(data);
+
+    if(!verifyContents(data)) {
+      // REQUISIÇÃO PARA O ENDPOINT DO TELEGRAM
+      localStorage.setItem('tabnews-post-monitor', JSON.stringify(data));
+      console.log("aqui né")
+      setMessage(SUCCESS_MESSAGE);
+      return;
+    } else {
+      console.log("aqui fora")
+
+      setMessage(ERROR_MESSAGE);
+    }
+
+  }
+
+  function verifyContents(object) {
+    return Object.values(object).some(property => property === null || property.length === 0);
+  }
 
   return (
     <main className={styles.main}>
-      <section className={styles.section}>
+      <section className={styles.formSection}>
         <div className={styles.container}>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={(event) => handleRegisterPostMonitor(event)}>
             <div className={styles.formContainer}>
               <div className={styles.telegramForm}>
                 <strong>Telegram</strong>
                 <div>
                   <label>Bot Key</label>
-                  <input type="text" placeholder="bot5723123438:AEEGSptRjacy_ZIojCrRjdaUUuChFM-lgb7kA" />
+                  <input 
+                    type="text" 
+                    placeholder="bot5723123438:AEEGSptRjacy_ZIojCrRjdaUUuChFM-lgb7kA" 
+                    onChange={(event) => setBotKey(event.target.value)} />
                 </div>
                 <div>
                   <label>Chat id</label>
-                  <input type="text" placeholder="@elopesnotificatabnews" />
+                  <input 
+                    type="text" 
+                    placeholder="@elopesnotificatabnews" 
+                    onChange={(event) => setChatId(event.target.value)} />
                 </div>
               </div>
               <div className={styles.tabnewsForm}>
                 <strong>Tabnews</strong>
                 <div>
                   <label>Username</label>
-                  <input type="text" placeholder="elopes" />
+                  <input 
+                    type="text" 
+                    placeholder="elopes" 
+                    onChange={(event) => setUsername(event.target.value)} />
                 </div>
                 <div>
                   <label>Post Slug</label>
-                  <input type="text" placeholder="notificador-tabnews-no-telegram" />
+                  <input 
+                    type="text" 
+                    placeholder="notificador-tabnews-no-telegram" 
+                    onChange={(event) => setPostSlug(event.target.value)} />
                 </div>
               </div>
             </div>
             <button type="submit">Eu quero ser notificado!</button>
+            <p>{message}</p>
           </form>
         </div>
+      </section>
+      <section className={styles.monitoredPostSection}>
+        
       </section>
     </main>
   )
